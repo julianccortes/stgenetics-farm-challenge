f9108ed1b3df8176815de4f57fc02300359506b2
Julian Camilo Cortes Chia <julianccortes@gmail.com>
2023-06-25 22:19:01 -0500

    Delete unused project

--- src/server/--STgenetics.Farm.Application/Interfaces/Repositories/IAnimalsRepositoryAsync.cs
+++
@@ -1,8 +0,0 @@
-﻿using STgenetics.Farm.Domain.Entities;
-
-namespace STgenetics.Farm.Application.Interfaces.Repositories
-{
-    public interface IAnimalsRepositoryAsync : IGenericRepositoryAsync<Animal, int>
-    {
-    }
-}
